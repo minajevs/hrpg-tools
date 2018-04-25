70b5be888f6e2f5ab5adf87657b54121327ee093
Dmitrijs Minajevs <d3x133@gmail.com>
2018-04-25 17:34:00 +0300

    Update HRPG Rift sound notification.user.js

--- HRPG Rift sound notification.user.js
+++ HRPG Rift sound notification.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         HRPG Rift sound notification
+// @name         HRPG sound notification
 // @namespace    http://heroesrpg.com/
-// @version      1.2
+// @version      1.3
 // @description  Little HRPG helper with features approved by Carl
 // @author       Code
 // @match        http://www.heroesrpg.com/*
@@ -11,6 +11,7 @@
 (function() {
     'use strict';
     var _savedSettings = localStorage.getItem('_savedSettings');
+    var _savedSettings_PM = localStorage.getItem('_savedSettings_PM');
     if(!_savedSettings){
         _reset();
     }
@@ -18,17 +19,20 @@
     $('#_notifierSettings').on('click', function(){
         showPopup();
         $('#popup-title').html('Sound notifier settings');
-        $('#popup-content').html('<p>What text (only Global) triggers sound alarm. Separate with ";":</p><br/><textarea id="_triggers" cols="50"></textarea><br/><input type="button" id="_save" value="Save"/> | <input type="button" id="_reset" value="Reset"/><div style="position:absolute;bottom:0;right:0;">Made by Code.</div>');
+        $('#popup-content').html('<p>What text (only Global) triggers sound alarm. Separate with ";":</p><br/><textarea id="_triggers" cols="50"></textarea><br/><input type="button" id="_save" value="Save"/> | <input type="button" id="_reset" value="Reset"/><br/><input id="_pmTrigger" type="checkbox"><label for="_pmTrigger">Play sound on PM</label><div style="position:absolute;bottom:0;right:0;">Made by Code. Special thanks to euphone.</div>');
         $('#_triggers').val(localStorage.getItem('_savedSettings'));
         $('#_save').on('click', function(){
             localStorage.setItem('_savedSettings', $('#_triggers').val());
+            localStorage.setItem('_savedSettings_PM', $('#_pmTrigger').checked);
         });
         $('#_reset').on('click', _reset);
     });
 
     function _reset(){
         localStorage.setItem('_savedSettings', 'A Rift will open in 5 minutes;Rift has opened!;');
+        localStorage.setItem('_savedSettings_PM', true);
         $('#_triggers').val(localStorage.getItem('_savedSettings'));
+        $('#_pmTrigger').checked = localStorage.getItem('_savedSettings_PM');
     }
 
     var _audio;
@@ -55,6 +59,11 @@
                         }
                     }
                 }
+                if(localStorage.getItem('_savedSettings_PM') && $.inArray(data.c[i].uid, ignored) < 0) {
+                    if(data.c[i].type == 50) {
+                        _play();
+                    }
+                 }
             });
         }
     }
